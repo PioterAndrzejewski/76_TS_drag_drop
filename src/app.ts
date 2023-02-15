e4615d660008ba4fa3c3671f20395c71848f88a5
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2023-02-15 23:14:10 +0100

    handle object drag

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+// Drag & Drop Interfaces
+interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent): void;
+}
+
+
+interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+}
+
 // Project Type
 enum ProjectStatus {
     Active, Finished
@@ -69,19 +82,19 @@ function validate(validatableInput: Validatable) {
     }
 
     if(validatableInput.minLength != null && typeof validatableInput.value === "string") {
-        isValid = isValid && validatableInput.value.length > validatableInput.minLength;
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
     }
 
     if(validatableInput.maxLength != null && typeof validatableInput.value === "string") {
-        isValid = isValid && validatableInput.value.length < validatableInput.maxLength;
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
     }
 
     if (validatableInput.min != null && typeof validatableInput.value === "number") {
-        isValid = isValid && validatableInput.value > validatableInput.min;
+        isValid = isValid && validatableInput.value >= validatableInput.min;
     }
 
     if (validatableInput.max != null && typeof validatableInput.value === "number") {
-        isValid = isValid && validatableInput.value < validatableInput.max;
+        isValid = isValid && validatableInput.value <= validatableInput.max;
     }
 
     return isValid;
@@ -129,9 +142,53 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 
 }
 
+// ProjectItem Class
+
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
+
+     private project: Project;
+
+     get persons() {
+        if (this.project.people === 1) {
+            return `1 person`
+        } else {
+            return `${this.project.people} persons`
+        }
+
+     }
+
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+
+        this.configure();
+        this.renderContent();
+    }
+
+    @autobind
+    dragStartHandler(event: DragEvent): void {
+        console.log(event)
+    }
+
+    dragEndHandler(event: DragEvent): void {
+        console.log(event)
+    }
+
+    configure() {
+        this.element.addEventListener('dragstart', this.dragStartHandler)
+        this.element.addEventListener('dragend', this.dragEndHandler)
+    }
+
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.persons + ` assigned`;
+        this.element.querySelector('p')!.textContent = this.project.description;
+    }
+}
+
 // ProjectList Class
 
-class ProjectList extends Component<HTMLDivElement, HTMLElement> {
+class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget{
     assignedProjects: Project[];
 
 
@@ -145,7 +202,27 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         this.renderContent();
     }
 
+    @autobind
+    dragOverHandler(_: DragEvent): void {
+        const listEl = this.element.querySelector('ul');
+        listEl!.classList.add('droppable');
+    }
+
+    dropHandler(_: DragEvent): void {
+        
+    }
+    
+    @autobind
+    dragLeaveHandler(_: DragEvent): void {
+        const listEl = this.element.querySelector('ul');
+        listEl!.classList.remove('droppable');
+    }
+
     configure(): void {
+        this.element.addEventListener('dragover', this.dragOverHandler);
+        this.element.addEventListener('dragleave', this.dragLeaveHandler);
+        this.element.addEventListener('drop', this.dropHandler);
+
         projectState.addListener((projects: Project[]) => {
             const relevantProjects = projects.filter(prj => {
                 if (this.type === 'active') {
@@ -170,9 +247,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         const listEl = document.getElementById(`${this.type}-projects-list`)!
         listEl.innerHTML = "";
         for (const prjItem of this.assignedProjects) {
-            const listItem = document.createElement('li');
-            listItem.textContent = prjItem.title;
-            listEl.appendChild(listItem);
+            new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
         }
     }
 
